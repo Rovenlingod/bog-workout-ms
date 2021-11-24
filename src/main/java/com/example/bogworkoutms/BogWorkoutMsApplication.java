e3d39ae14c7219bbc2463530f7224362b9cc660b
Daniil Kovalyov <kovalyovd21@gmail.com>
2021-11-24 19:16:28 +0300

    Initial commit

---
+++ src/main/java/com/example/bogworkoutms/BogWorkoutMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bogworkoutms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogWorkoutMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BogWorkoutMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bogworkoutms/BogWorkoutMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bogworkoutms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogWorkoutMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
